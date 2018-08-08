e236806b90d63658bc8ef95a7beae9d0e298c8e2
sagar <sagarsharma631@gmail.com>
2018-08-08 23:32:14 +0530

    added insert method [need to make doc for this method], completed LR rotation and RL rotation

--- src/balance_base.hpp
+++ src/balance_base.hpp
@@ -21,22 +21,30 @@ struct balance_node{
 	T m_Value;
 	balance_node<T> *m_pright;
 	uint8_t m_iBalFac;	// Balance factor of the node
-	uint8_t m_RightHt;	// Right height of the node
-	uint8_t m_LeftHt;	// Left height of the node
+	uint8_t m_iRightHt;	// Right height of the node
+	uint8_t m_iLeftHt;	// Left height of the node
 	balance_node(balance_node<T> *left = NULL, balance_node<T> *right = NULL, T value, uint8_t factor = 0, uint8_t rtht = 0, uint8_t ltht = 0) : m_pleft(left),
 		m_pright(right),
 		m_Value(value),
 		m_iBalFac(factor),
-		m_RightHt(rtht),
-		m_LeftHt(ltht){
+		m_iRightHt(rtht),
+		m_iLeftHt(ltht){
 	}
 };
 
 template<typename T>
 class balance_base : public tree_base{
 	private:
+
+	// Temporary variables used in the class. Always flush these variables before use as they could contain old values
+	bool m_bBoolTemp;
+	
 	public:
 	
+	void flushTempBool(){
+		m_bBoolTemp = false;
+	}
+
 	// This method decides which rotation to do
 	ROTATION which_rotation(){
 	}
@@ -67,10 +75,57 @@ class balance_base : public tree_base{
 		return root;
 	}
 
+	/*
+	 *	The following method inserts the node and also updates the right height or the left height of the node.
+	 *
+	 *	MUST DO - Need to make document for the following method as following method is complex and difficult to understand
+	 *
+	 */
+	void insert_node(balance_node<T> *nd_ins,balance_node<T> *nd_ptr){
+		if(nd_ins->m_Value < nd_ptr->m_Value){
+			if(nd_ptr->m_pleft == NULL){
+				nd_ptr->m_pleft = nd_ins;
+				if(nd_ptr->m_pright == NULL){
+					m_bBoolTemp = true;
+					++nd_ptr->m_iLeftHt;
+					return;
+				}
+			}
+			insert_node(nd_ins, nd_ptr->m_pleft);
+			if(m_bBoolTemp)
+				++nd_ptr->m_iLeftHt;
+		}
+		else if(nd_ins->m_Value > nd_ptr->m_Value){
+			if(nd_ptr->m_pright == NULL){
+				nd_ptr->m_pright = nd_ins;
+				if(nd_ptr->m_pleft == NULL){
+					m_bBoolTemp = true;
+					++nd_ptr->m_iRightHt;
+					return;
+				}
+			}
+			insert_node(nd_ins, nd_ptr->m_pright);
+			if(m_bBoolTemp)
+				++nd_ptr->m_iRightHt;
+		}
+	}	
+
 	void do_left_right(node<T> *sub_root){
+		balance_node<T> *tempNode = do_single_left(sub_root->m_pleft);
+		if(tempNode){
+			sub_root->m_pleft = tempNode;
+			do_single_right(sub_root);
+		}
+		return;
 	}
 
 	void do_right_left(node<T> *sub_root){
+		balance_node<T> *tempNode = do_single_left(sub_root->m_pright);
+		if(tempNode){
+			sub_root->m_pright = tempNode;
+			do_single_left(sub_root);
+		}
+		return;
 	}
 	
 };
