1a7027888093d26f2b3e7b74043d5dff909b99a0
sagarsharma631 <40322628+sagarsharma631@users.noreply.github.com>
2019-08-06 11:09:06 -0500

    Create longestAlternatingSubarray.cpp

---
+++ longestAlternatingSubarray.cpp
@@ -0,0 +1,44 @@
+/*
+
+Given an array of N including positive and negative numbers only.
+The task is to find the length of the longest alternating (means negative-positive-negative or positive-negative-positive) subarray present in the array.
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+bool isAlternateIndex(int arr[], int index){
+    bool result = false;
+    if(arr[index] * arr[index+1] < 0){
+        result = true;
+    }
+    return result;
+}
+
+int findLongestAlternate(){
+	int arr2[] = {1, -5, 1, -5};
+    int arr[] = {-5,-1,-1,2,-2,-3};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int longest = 0;
+    int windowLength = 0;
+    
+    for(int i=0;i<size;i++){
+        if(isAlternateIndex(arr,i)){
+            ++windowLength;
+            if(windowLength > longest){
+                longest = windowLength;
+            }
+        }
+        else{
+            windowLength = 0;
+        }
+    }
+    return longest+1;
+}
+
+int main(int argc, char *argv[]){
+    cout<<findLongestAlternate();
+    return 0;
+}
