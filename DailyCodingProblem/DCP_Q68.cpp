7fba4960ceb00ad85a38d2a597a0772a7800fd11
sagarsharma631 <40322628+sagarsharma631@users.noreply.github.com>
2020-01-30 09:51:26 +0530

    Diagonal Bishop problem Completed
    Done by calculating the slope, if my slope is 1 than they are in diagonal.

--- DailyCodingProblem/DCP_Q68.cpp
+++ DailyCodingProblem/DCP_Q68.cpp
@@ -24,3 +24,61 @@ You should return 2, since bishops 1 and 3 attack each other, as well as bishops
 
 */
 
+#include"stdafx.h"
+#include<iostream>
+#include<vector>
+#include<utility>
+
+// Uncomment if you want ot print some important variables.
+//#define DEBUG	
+
+using namespace std;
+
+// The below method calculate and tells whether the slope between two points is a whole number or not.
+bool isSlopeWholeNumber(const pair<int,int> &bloc1, const pair<int,int> &bloc2) {
+	bool retVal = false;
+	int numerator = abs(bloc2.second - bloc1.second);
+	int denominator = abs(bloc2.first - bloc1.first);
+	// If my numberator would be equal to denominator, than it means my other bishop is present in a straight line to another bishop.
+	if (numerator == denominator) {
+		retVal = true;
+	}
+	return retVal;
+}
+
+int computeAllBishopsAttack(const vector<pair<int,int>> &bishopLoc) {
+	int retVal = 0;
+	if (bishopLoc.empty()) {
+		return retVal;
+	}
+
+	int indexUpper = bishopLoc.size();
+	for (int i = 0;i < indexUpper;++i) {
+		if (i == indexUpper - 1) {
+			// need to break the loop as j will go out of bound. Need not check the condition of whole number slope for last element.
+			break;
+		}
+		for (int j = i + 1;j < indexUpper;++j) {
+			if (isSlopeWholeNumber(bishopLoc[i], bishopLoc[j])) {
+				++retVal;
+			}
+		}
+	}
+	return retVal;
+}
+
+int main(int argc, char *Argv[]) {
+	// the following vector contains the locations of bishop.
+	vector<pair<int, int>> bishopLoc;
+	bishopLoc.push_back(make_pair(0, 0));
+	bishopLoc.push_back(make_pair(1, 2));
+	bishopLoc.push_back(make_pair(2, 2));
+	bishopLoc.push_back(make_pair(4,0));
+	computeAllBishopsAttack(bishopLoc);
+#ifdef DEBUG
+	cout << computeAllBishopsAttack(bishopLoc);
+	getchar();
+#endif // DEBUG
+	
+	return 0;
+}
