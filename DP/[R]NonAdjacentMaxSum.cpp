2e779941f2bbc16df7fd711b529bdbd28cd2d738
sagarsharma631 <40322628+sagarsharma631@users.noreply.github.com>
2020-02-14 14:27:55 +0530

    Just Modified date and e.g.

--- DP/[R]NonAdjacentMaxSum.cpp
+++ DP/[R]NonAdjacentMaxSum.cpp
@@ -2,11 +2,12 @@
 
 AUTHOR - SAGAR SHARMA
 
-DATE - 14/-2/2020
+DATE - 14/2/2020
 
 Question - 
 Calcualte maximum sum in an array(containing all numbers >= 0), the sum should not includ adjacent numbers.
-
+e.g.  {1,2,9,4,5,0,4,11,6};
+o/p - 26
 */
 #include<iostream>
 
